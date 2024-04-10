b0639ad4cb52a0dcf96c39ed7570c0f59c8fdc33
pannag kumaar <pannag2003@gmail.com>
2024-04-10 09:55:41 +0530

    add main.java

---
+++ src/java_project/main.java
@@ -0,0 +1,102 @@
+package java_project;
+
+//import java.io.*;
+
+public class main {
+
+    //VARIABLES ================================================================
+    public static User currentUser = new User();
+    public static File currentUserFile;
+    public static Database database;
+
+//GLOBAL METHODS ===============================================================
+    //CHECK USER INPUT - DOUBLE ================================================
+    public static boolean doubleIsParsable(String input) {
+        boolean parsable = true;
+        try {
+            Double.parseDouble(input);
+        } catch (Exception e) {
+            e.printStackTrace();
+            parsable = false;
+        }
+        return parsable;
+    }
+
+    //CHECK USER INPUT - INT ===================================================
+    public static boolean intIsParsable(String input) {
+        boolean parsable = true;
+        try {
+            Integer.parseInt(input);
+        } catch (Exception e) {
+            e.printStackTrace();
+            parsable = false;
+        }
+        return parsable;
+    }
+
+    //WRITE OBJECT TO FILE METHOD ==============================================
+    public static void WriteObjectToFile(Object user, File xFile) {
+        try {
+            try {
+                //Output Stream Setup
+                FileOutputStream fileOut = new FileOutputStream(xFile);
+                ObjectOutputStream out = new ObjectOutputStream(fileOut);
+
+                //Write the Object onto the Output Stream
+                out.writeObject(user);
+                out.close();
+                System.out.println("The user was succesfully written to a file");
+
+            } catch (Exception exc1) {
+                exc1.printStackTrace();
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    //READ OBJECT FROM SPECIFIED FILE ==========================================
+    public static Object ReadObjectFromFile(File xFile) {
+        try {
+            try {
+                if (!xFile.exists()) {
+                    System.out.println("File does not exist");
+                    return null;
+                }
+
+                //Input Stream Setup
+                FileInputStream fileIn = new FileInputStream(xFile);
+                ObjectInputStream in = new ObjectInputStream(fileIn);
+
+                //Save the read object
+                Object obj = in.readObject();
+
+                //Debug code
+                System.out.println("The Object has been read from the file");
+
+                //Close the Input Stream
+                in.close();
+
+                //Return Information
+                return obj;
+
+            } catch (Exception exc1) {
+                exc1.printStackTrace();
+                return null;
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+//MAIN PROJECT METHOD ==========================================================
+    public static void main(String[] args) {
+
+        //OPENING FIRST FORM ===================================================
+        LogInScreen LogInWindow = new LogInScreen();
+        LogInWindow.setVisible(true);
+    }
+}
